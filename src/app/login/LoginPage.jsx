f0e2437415f91761f301bf811dcac36b55886633
lkarpenko <lkarpenko@alteryx.com>
2019-07-11 17:06:46 +0300

    create a new project

---
+++ src/app/login/LoginPage.jsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import CardContent from "@material-ui/core/CardContent";
+import Icon from "@material-ui/core/Icon";
+import Typography from "@material-ui/core/Typography";
+import Email from "@material-ui/icons/Email";
+import People from "@material-ui/icons/People";
+import Header from "components/Header/Header.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import GridItem from "components/Grid/GridItem.jsx";
+import Button from "components/CustomButtons/Button.jsx";
+import Card from "components/Card/Card.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import CardFooter from "components/Card/CardFooter.jsx";
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+
+import loginPageStyle from "assets/jss/material-kit-react/views/loginPage.jsx";
+
+import image from "assets/img/bg7.jpg";
+
+function LoginPage({ classes, ...rest }) {
+  const [cardAnimaton, setCardAnimaton] = useState("cardHidden");
+  useEffect(() => {
+    // we add a hidden class to the card and after 700 ms we delete it and the transition appears
+    setTimeout(
+      () => setCardAnimaton(""),
+      700
+    );
+  });
+
+  return (
+    <div>
+      <Header
+        absolute
+        color="transparent"
+        brand="Material Kit React"
+        rightLinks={<HeaderLinks />}
+        {...rest}
+      />
+      <div
+        className={classes.pageHeader}
+        style={{
+          backgroundImage: "url(" + image + ")",
+          backgroundSize: "cover",
+          backgroundPosition: "top center"
+        }}
+      >
+        <div className={classes.container}>
+          <GridContainer justify="center">
+            <GridItem xs={12} sm={12} md={4}>
+              <Card className={classes[cardAnimaton]}>
+                <CardContent>
+                  <CardBody>
+                  <Typography variant={"h5"}>
+                    Login using creds
+                  </Typography>
+                  <form className={classes.form}>
+                    <CustomInput
+                      labelText="First Name..."
+                      id="first"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: "text",
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <People className={classes.inputIconsColor} />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                    <CustomInput
+                      labelText="Email..."
+                      id="email"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: "email",
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <Email className={classes.inputIconsColor} />
+                          </InputAdornment>
+                        )
+                      }}
+                    />
+                    <CustomInput
+                      labelText="Password"
+                      id="pass"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      inputProps={{
+                        type: "password",
+                        endAdornment: (
+                          <InputAdornment position="end">
+                            <Icon className={classes.inputIconsColor}>
+                              lock_outline
+                            </Icon>
+                          </InputAdornment>
+                        ),
+                        autoComplete: "off"
+                      }}
+                    />
+                  </form>
+                  </CardBody>
+                    <CardFooter className={classes.cardFooter}>
+                      <Button simple color="primary" size="lg">
+                        Get started
+                      </Button>
+                    </CardFooter>
+                </CardContent>
+              </Card>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+LoginPage.propTypes = {
+  classes: PropTypes.object
+};
+
+export default withStyles(loginPageStyle)(LoginPage);
